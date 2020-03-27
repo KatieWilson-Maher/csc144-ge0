c653d6a764d6cf1cdcd7dbee486d87e0cd1ae74a
katie <katie@DESKTOP-CICGU7F>
2020-03-26 20:45:44 -0500

    Two new methods.

--- src/searchandsort/SearchAndSort.java
+++ src/searchandsort/SearchAndSort.java
@@ -3,7 +3,13 @@ package searchandsort;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Collections;
 
+
+/**
+ * @author katie
+ * @version 3/26/20
+ */
 public class SearchAndSort {
 
     private static Random rng = new Random();
@@ -90,13 +96,96 @@ public class SearchAndSort {
     // TO-DO: Define a method that sorts a list
     // of integers using the selection sort algorithm.
     // TO-DO: Define a method that sorts a list
-    // of integers using the insertion sort algorithm.
-    // TO-DO: Define a method that sorts a list
     // of integers using the merge sort algorithm.
+    
+    /**
+    *Performs the Insertion Sort Method on an ArrayList
+    * 
+     * @param list an ArrayList to be sorted
+    * @return ArrayList sorted
+    */
+    public static ArrayList insertionSort(ArrayList list){
+        int size = list.size();
+        for (int i = 0; i < size-1; i++){
+            int min = (int) list.get(i);
+            int next = (int) list.get(i+1);
+            if (min>next){
+                Collections.swap(list, i, i+1);
+            }
+        }
+        return list;
+    }
+    /**
+     * Splits an ArrayList into two ArrayLists
+     * 
+     * @param list ArrayList to be split
+     * @return ArrayList split
+     */
+    public static ArrayList splitList(ArrayList list){
+        int size = list.size();
+        int firstHalf;
+        int secondHalf;
+        if (size%2 == 0){
+            firstHalf = size/2;
+            secondHalf = firstHalf;
+        }
+        else {
+            firstHalf = size/2;
+            secondHalf = firstHalf + 1;
+        }
+        ArrayList<Integer> firstHalfList = new ArrayList<>();
+        for (int i = 0; i < firstHalf; i++){
+            firstHalfList.add((Integer) list.get(i));
+        }
+        ArrayList<Integer> secondHalfList = new ArrayList<>();
+        for (int i = firstHalf; i < size; i++){
+            secondHalfList.add((Integer) list.get(i));
+        }
+        ArrayList<ArrayList> bothLists = new ArrayList<>();
+        bothLists.add(firstHalfList);
+        bothLists.add(secondHalfList);
+        return bothLists;
+    }
+    
+//    public static void mergeSort(ArrayList list){
+//        int size = list.size();
+//        ArrayList<ArrayList> elements = new ArrayList<>();
+//        for (int i = 0; i<2; i++){
+//            //System.out.println(list);
+//            ArrayList split = splitList(list);
+//            //System.out.println(split);
+//           ArrayList newList = (ArrayList) split.get(i);
+//           
+//            //System.out.println(list);
+//            split.remove(0);
+//            size = newList.size();
+//            //System.out.println(size);
+//            //System.out.println(list);
+//            elements.add(newList);
+//            System.out.println("newList: "+newList);
+//        }
+//        
+//        System.out.println("list: "+list);
+//        System.out.println("elements: "+elements);
+//    }
+    
     public static void main(String[] args) {
-        System.out.println("Searching and sorting algorithms");
+       // System.out.println("Searching and sorting algorithms");
 
         // TO-DO: Add code that tests the searching and sorting
         // methods.
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(4);
+        list.add(2);
+        list.add(25);
+        list.add(5);
+        list.add(20);
+        list.add(40);
+
+        System.out.println("insertionSort: "+insertionSort(list));
+
+        System.out.println("splitList: "+splitList(list));
+        
+        ///mergeSort(list);
     } // main( String [] )
 } // SearchAndSort
